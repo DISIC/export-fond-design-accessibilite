973882e08aa6022db4d169649e4c5f6e984b9cc7
Adrien Boutigny <adrien@slash-tmp.dev>
2023-04-05 12:00:19 +0200

    add function to upload to airtable

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-import fetch from "node-fetch";
+import got from "got";
 
-const { DS_API_URL, DS_API_TOKEN } = process.env;
+const {
+  DS_API_TOKEN,
+  AIRTABLE_BASE_ID,
+  AIRTABLE_TABLE_ID,
+  AIRTABLE_ACCESS_TOKEN,
+} = process.env;
 
 const query = `
   query Request {
@@ -24,24 +29,66 @@ const query = `
   }
 `;
 
-async function main() {
-  const data = await fetch(DS_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      Authorization: `Bearer ${DS_API_TOKEN}`,
-    },
-    body: JSON.stringify({
-      query,
-      variables: {},
+/** Fetch data from demarches-simplifiees.fr API */
+async function fetchDosiers() {
+  console.log("⏳ Fetching data...");
+  return got
+    .post("https://www.demarches-simplifiees.fr/api/v2/graphql", {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${DS_API_TOKEN}`,
+      },
+      json: {
+        query,
+        variables: {},
+      },
+    })
+    .json();
+}
+
+function transformDossierData(data) {
+  console.log("⚙️ Transforming data...");
+  return {
+    records: data.data.demarche.dossiers.nodes.map((dossier) => {
+      // console.log(dossier);
+      return {
+        fields: {
+          ID: dossier.id,
+          "Porteur du projet": dossier.champs.find(
+            (c) => c.label === "Nom et prénom du porteur de projet"
+          ).stringValue,
+          "Fonction du porteur de projet": dossier.champs.find(
+            (c) => c.label === "Fonction du porteur de projet"
+          ).stringValue,
+          "Email du porteur de projet": dossier.champs.find(
+            (c) => c.label === "Email du porteur de projet"
+          ).stringValue,
+        },
+      };
     }),
-  }).then((res) => res.json());
+  };
+}
 
-  // console.dir(data, { depth: null });
+async function uploadDossiers(data) {
+  console.log("Uploading data...");
 
-  const dossierCount = data.data.demarche.dossiers.nodes.length;
-  console.log("Succesfully fetched data for", dossierCount, "forms");
+  await got.post(
+    `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_ID}`,
+    {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_ACCESS_TOKEN}`,
+      },
+      json: data,
+    }
+  );
+}
+
+async function main() {
+  const data = await fetchDosiers();
+  const transformedData = transformDossierData(data);
+  await uploadDossiers(transformedData);
+
+  console.log("✅ Sucessfully updated airtable data");
 }
 
 main();
